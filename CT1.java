2d3b2f96ed39ee0a1235ba52e9cf2c0d707eac53
Karumasensei <mcawesome.eighmy@gmail.com>
2024-09-19 11:23:19 -0700

    Add files via upload

---
+++ CT1.java
@@ -0,0 +1,17 @@
+public class CT1 {
+    public static void main(String[] args) {
+        // Declare variables for restaurant information
+        String restaurantName = "Yummy Yummy in Your Tummy";
+        String businessAddress = "123 Foodie Lane";
+        String city = "Spokane";
+        String state = "WA";
+        String zipCode = "99201";
+
+        // Print the restaurant information
+        System.out.println("Restaurant Name: " + restaurantName);
+        System.out.println("Business Address: " + businessAddress);
+        System.out.println("City: " + city);
+        System.out.println("State: " + state);
+        System.out.println("Zip Code: " + zipCode);
+    }
+}
