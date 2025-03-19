738d19d201213375fad45cf880ab9dd4755d2149
Karumasensei <mcawesome.eighmy@gmail.com>
2025-03-18 23:54:02 -0700

    Add files via upload
    Note pad Java file

---
+++ CT1.java
@@ -0,0 +1,17 @@
+public class CT1 {
+    public static void main(String[] args) {
+        // Declare variables for restaurant information
+        String restaurantName = " Whatever";
+        String businessAddress = "123 My Wife's Answer";
+        String city = "Most of the Time";
+        String state = "ISK";
+        String zipCode = "A80UT F00D";
+
+        // Print the restaurant information
+        System.out.println("Restaurant Name: " + restaurantName);
+        System.out.println("Business Address: " + businessAddress);
+        System.out.println("City: " + city);
+        System.out.println("State: " + state);
+        System.out.println("Zip Code: " + zipCode);
+    }
+}
